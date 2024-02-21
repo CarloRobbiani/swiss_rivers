71639a133a32b10226c55cd01a03c7c0327bab6f
CarloRobbiani <79840218+CarloRobbiani@users.noreply.github.com>
2024-02-21 10:34:55 +0100

    Updated torch plot to have colours
    Color is red if there is a missing value blue if not.

--- torch_plot.py
+++ torch_plot.py
@@ -1,20 +1,43 @@
 import matplotlib.pyplot as plt
 from my_graph_reader import ResourceRiverReaderFactory
+import os
+import pandas as pd
 
 
 
 #Function to plot the river data
 def plot_river_data(data, data_edges):
     # Plot the river nodes
-    x = data[:,0].numpy()
-    y = data[:,1].numpy()
-    plt.scatter(x, y, color='blue', label='River Nodes')
+    x = data[:,0]
+    y = data[:,1]
+    stations = data[:,2].numpy()
+    files_flow = os.listdir("hydro_data\Flow")
+    files_temp = os.listdir("hydro_data\Temp")
+    values = []
+    
+    #temporary get first line and check for missing data
+    for station in stations:
+        file = [file for file in files_flow if file.startswith(str(station))]
+        file.extend([file for file in files_temp if file.startswith(str(station))])
+
+        
+        if len(file) > 0:
+            file_path = [f"hydro_data\Flow\{f}"if "Abfl" in f else "hydro_data\Temp\{f}" if "Wasser" in f else "" for f in file]
+            df = pd.read_csv(file_path[0], delimiter=";", skiprows=8, encoding="iso-8859-1")
+            values.append(pd.isna(df["Wert"].iloc[0]))
+        else:
+            values.append(1)
+
+
+    colors = ["red" if value == 1 else "blue" for value in values]
+
+    plt.scatter(x, y, color=colors, label='River Nodes')
 
     # Plot the river edges
     for i in range(data_edges.shape[1]):
         start = data_edges[0, i]
         end = data_edges[1, i]
-        plt.plot([x[start], x[end]], [y[start], y[end]], color='red')
+        plt.plot([x[start], x[end]], [y[start], y[end]], color='black')
 
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
